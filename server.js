1e94cb0d92497b416bbe7cef16c8c2686013e75f
vallal0011@gmail.com <vallal0011@gmail.com>
2024-11-01 19:45:35 +0530

    modifications in startup

--- server.js
+++ server.js
@@ -9,8 +9,10 @@ const port = process.env.PORT || 3003;
 
 app.use(express.json());
 
+MONGO_URI='mongodb+srv://panchadsharamanusha:1234@cluster0.38fjgnz.mongodb.net/solaxRealData'
+
 // MongoDB connection
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(MONGO_URI , { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('Connected to MongoDB'))
     .catch(err => console.error('Could not connect to MongoDB', err));
 
